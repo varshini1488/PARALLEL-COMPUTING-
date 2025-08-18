39d155fc0e0c47869a63e77d814c85e64770cdab
varshiniacharya <153392139+varshini1488@users.noreply.github.com>
2025-08-18 10:22:42 +0530

    Add files via upload

---
+++ cie1.c
@@ -0,0 +1,12 @@
+// write an open mp program which launhces a parallel and it had each tread print its ID and team size 
+#include <stdio.h>
+#include <omp.h>
+int main() {
+    #pragma omp parallel
+    {
+        int thread_id = omp_get_thread_num();
+        int num_threads = omp_get_num_threads();
+        printf("Thread ID: %d, Team Size: %d\n", thread_id, num_threads);
+    }
+    return 0;
+}
